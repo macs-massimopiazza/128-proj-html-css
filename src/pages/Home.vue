4adfe3eab7c4369e106873c7a0bf1d1df2301015
macs-massimo-piazza <me@massimopiazza.com>
2024-07-16 15:26:43 +0200

    feat: add subtitle, edit h1 in home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,8 @@ export default {
 </script>
 
 <template>
-  <h1>Sono una pagina home</h1>
+  <h1>Questa è la mia pagina home, che bello</h1>
+  <h2>Vero è proprio bello</h2>
 </template>
 
 <style scoped>
