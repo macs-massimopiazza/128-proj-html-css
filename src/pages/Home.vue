08842bc9b8c07b3ac49c374a955f7efd8cbe08e6
Simone Testa <sim.testa97@gmail.com>
2024-07-16 15:50:33 +0200

    feat: add h4 in home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,6 +8,7 @@ export default {
   <h1>Questa è la mia pagina home, che bello</h1>
   <h2>Vero è proprio bello</h2>
   <h3>Ciao Massimo!</h3>
+  <h4>Questo è un nuovo h4!</h4>
 </template>
 
 <style scoped></style>
